f8b96e846a5d627917da28dfb1400b32e0560cbb
Alan Semenov <ase@enonic.com>
2015-07-16 15:34:43 +0200

    XP-1044 GA - Create html file in the GA app * Added html/js for showing GA key (if it's defined under site settings) or error message (if it's not)

--- src/main/resources/app/widgets/traffic-report/traffic-report.js
+++ src/main/resources/app/widgets/traffic-report/traffic-report.js
@@ -1,3 +1,27 @@
-exports.get = function() {
+var portal = require('/lib/xp/portal');
+var thymeleaf = require('/lib/xp/thymeleaf');
 
+exports.get = function() {
+    var content = portal.getContent();
+    var view = resolve('traffic-report.html');
+    var siteConfigs = portal.getSite().siteConfigs;
+    var googleUA;
+
+    log.info("Showing content of Google Analytics app");
+
+    if (siteConfigs["com.enonic.app.ga"]) {
+        googleUA = siteConfigs["com.enonic.app.ga"].googleUA;
+
+        log.info("GA key is: " + googleUA);
+    }
+
+    var params = {
+        content: content,
+        googleUA: googleUA
+    };
+
+    return {
+        contentType: 'text/html',
+        body: thymeleaf.render(view, params)
+    };
 };

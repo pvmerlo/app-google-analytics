8b05792a36e71b8b66d0623f40d9a33ed7731f52
Alan Semenov <ase@enonic.com>
2015-07-30 16:11:14 +0200

    XP-1073 GA - Call authentication service to get access token * Proper way to resolve REST service url

--- src/main/resources/site/widgets/ga-report/ga-report.js
+++ src/main/resources/site/widgets/ga-report/ga-report.js
@@ -2,17 +2,14 @@ var portal = require('/lib/xp/portal');
 var thymeleaf = require('/lib/xp/thymeleaf');
 
 function handleGet(req) {
-    var content = portal.getContent();
-    var serviceUrl = req.baseUrl + content._path + "/_/rest/google-analytics/authenticate";
+    var serviceUrl = portal.restServiceUrl({
+        path: 'google-analytics/authenticate'
+    });
     var view = resolve('ga-report.html');
-
-    var params = {
-        serviceUrl: serviceUrl
-    };
 
     return {
         contentType: 'text/html',
-        body: thymeleaf.render(view, params),
+        body: thymeleaf.render(view, {}),
         pageContributions: {
             bodyEnd: [
                 '<script src="' + portal.assetUrl({path: 'js/embed-api.js'}) + '" type="text/javascript"></script>',

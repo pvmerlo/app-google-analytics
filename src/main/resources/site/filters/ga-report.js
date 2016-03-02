aaf064441dbc8f70a1f24f5ba70c074e6c3d3c20
selbekk <kristofer.selbekk@bekk.no>
2016-03-02 12:22:33 -0800

    Simplify the code a bit, for easier readability

--- src/main/resources/site/filters/ga-report.js
+++ src/main/resources/site/filters/ga-report.js
@@ -5,30 +5,33 @@ exports.responseFilter = function (req, res) {
     var trackingID = siteConfig['trackingId'] || '';
     var enableTracking = siteConfig['enableTracking'] || false;
 
-    if (trackingID != '' && enableTracking) {
-        // Only add snippet if in live mode
-        if (req.mode === 'live') {
-            var snippet = '<!-- Google Analytics -->';
-            snippet += '<script>';
-            snippet += '(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){';
-            snippet += '(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),';
-            snippet += 'm=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)';
-            snippet += '})(window,document,\'script\',\'//www.google-analytics.com/analytics.js\',\'ga\');';
-            snippet += 'ga(\'create\', \'' + trackingID + '\', \'auto\');';
-            snippet += 'ga(\'send\', \'pageview\');';
-            snippet += '</script>';
-            snippet += '<!-- End Google Analytics -->';
-
-            var headEnd = res.pageContributions.headEnd;
-            if(!headEnd) {
-                res.pageContributions.headEnd = [];
-            }
-            else if(headEnd instanceof String) {
-                res.pageContributions.headEnd = [ headEnd ];
-            }
-
-            res.pageContributions.headEnd.push(snippet);
-        }
+    if(!trackingID || !enableTracking) {
+        return res;
     }
+
+    if(req.mode !== 'live') {
+        return res;
+    }
+
+    var snippet = '<!-- Google Analytics -->';
+    snippet += '<script>';
+    snippet += '(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){';
+    snippet += '(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),';
+    snippet += 'm=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)';
+    snippet += '})(window,document,\'script\',\'//www.google-analytics.com/analytics.js\',\'ga\');';
+    snippet += 'ga(\'create\', \'' + trackingID + '\', \'auto\');';
+    snippet += 'ga(\'send\', \'pageview\');';
+    snippet += '</script>';
+    snippet += '<!-- End Google Analytics -->';
+
+    var headEnd = res.pageContributions.headEnd;
+    if(!headEnd) {
+        res.pageContributions.headEnd = [];
+    }
+    else if(headEnd instanceof String) {
+        res.pageContributions.headEnd = [ headEnd ];
+    }
+
+    res.pageContributions.headEnd.push(snippet);
     return res;
 };
